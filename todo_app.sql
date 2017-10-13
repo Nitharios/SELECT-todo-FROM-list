9b1eec04e0cf74a8aafcac5f47b36df3bebd2ace
Nathan N <Nitharios@users.noreply.github.com>
2017-10-12 15:45:03 -1000

    More stuff working!

--- todo_app.sql
+++ todo_app.sql
@@ -1,21 +1,32 @@
 -- Sanity Check!
 
 -- drops db if it exists
--- DROP DATABASE IF EXISTS todo_app;
+DROP DATABASE IF EXISTS todo_app;
 -- -- delete a user if it exists
--- DROP USER IF EXITS michael;
+DROP USER IF EXISTS michael;
 -- -- create a user named michael
--- CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
+CREATE USER michael 
+WITH ENCRYPTED PASSWORD 'stonebreaker';
 -- -- create a database
--- CREATE DATABASE todo_app;
+CREATE DATABASE todo_app;
 -- -- connect to todo_app
--- -- in psql: \c [database name]
+\c todo_app;
 -- create a table named tasks
 CREATE TABLE tasks (
-  id            SERIAL PRIMARY KEY  NOT NULL,   
+  id            SERIAL              NOT NULL   PRIMARY KEY,   
   title         varchar(255)        NOT NULL,
   description   text,
   created_at    timestamp           NOT NULL   DEFAULT now(),
   update_at     timestamp,
   completed     boolean             NOT NULL   DEFAULT false
 );
+-- remove column completed
+-- add column completed_at timestamp null default now()
+-- change updated_at to not allow null and to have default of null()
+ALTER TABLE tasks
+  DROP COLUMN completed,
+  ADD COLUMN completed_at timestamp NULL DEFAULT NULL,
+  ALTER COLUMN update_at SET NOT NULL,
+  ALTER COLUMN update_at SET DEFAULT now();
+
+-- create a new task
